b03fc03e2c21b54e36fb983f17346777adc7b0cb
IT <shahadal0hamyanu=i@outlook.com>
2024-10-31 14:07:35 +0300

    edit

---
+++ Test/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+
+    int sum = a + b;
+    printf("Difference: %d\n", sum);
+
+    return 0;
+}
+
